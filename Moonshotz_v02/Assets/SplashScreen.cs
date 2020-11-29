eff83457e23f11cc97c29bbdf8853c6cfa70b010
jamesh-uk <james.havers@gmail.com>
2020-11-29 22:16:53 +0000

    Added Title Screen

---
+++ Moonshotz_v02/Assets/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	    StartCoroutine(WaitForNextScene());
+    }
+	
+	IEnumerator WaitForNextScene()
+	{
+		yield return new WaitForSeconds(5);
+		SceneManager.LoadScene(1);
+	}
+}
